57b02abd772e75d09d231381bb6be0766d002fa6
Davidson Francis <davidsondfgl@gmail.com>
2026-06-04 22:15:04 -0300

    Testing CI cache

--- aix-user.c
+++ aix-user.c
@@ -1,7 +1,7 @@
 /**
  * aix-user: a public-domain PoC/attempt to run 32-bit AIX binaries
  * on Linux via Unicorn, same idea as 'qemu-user', but for AIX+PPC
- * Made by Theldus, 2025
+ * Made by Theldus, 2025-2026
  */
 
 #include <stdio.h>
